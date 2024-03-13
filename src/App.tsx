983d05ecca8e5247dd6d88079fe492791d3e7e2d
MarcelinoGNeto <marcelino.gneto@gmail.com>
2024-03-12 22:57:50 -0300

    initial configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+type Location = {
+  id: string;
+  name: string;
+  description: string;
+  photo: string;
+};
 
 function App() {
+  const GET_LOCATIONS = gql`
+    query GetLocations {
+      locations {
+        id
+        name
+        description
+        photo
+      }
+    }
+  `;
+
+  function DisplayLocations() {
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.locations.map(({ id, name, description, photo }: Location) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
     </div>
   );
 }
